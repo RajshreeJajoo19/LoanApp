daf137f2a600f102865a8d640d028408ffb3b083
Rajshree Jajoo <rajshree.jajoo@sigmainfo.net>
2024-01-12 18:56:28 +0530

    cosmetic changes

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,8 +1,8 @@
 class Loan < ApplicationRecord
 
-validates :name, presence: true, length: { minimum: 6, maximum: 20 }
-validates :purpose, presence: true
-validates :amount, presence: true, length: { minimum: 6, maximum: 20 }
-validates :ssn, presence: true, length: { minimum: 6, maximum: 20 }
-
+    validates :name, presence: true, length: { minimum: 6, maximum: 20 }
+    validates :purpose, presence: true
+    validates :amount, presence: true, length: { minimum: 6, maximum: 20 }
+    validates :ssn, presence: true, length: { minimum: 6, maximum: 20 }
+    validates :email_address, presence: true, uniqueness: true
 end
