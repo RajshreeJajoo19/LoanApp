4b4a2c251af6ab2a6f5d9deb1dbc2e136e3cc0c1
Rajshree Jajoo <rajshree.jajoo@sigmainfo.net>
2024-01-15 15:06:20 +0530

    cosmetic changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,11 @@ class PagesController < ApplicationController
     def about
     end
 
+    #ajax functionality
+
+    def ajax_request
+        render json: { message: 'Hello from the server!' }
+    end
+
+
 end
---
+++ app/javascript/ajax_example.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/ajax_request", true);
+  
+    xhr.onload = function() {
+      if (xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log(response.message);
+      }
+    };
+  
+    xhr.send();
+  });
